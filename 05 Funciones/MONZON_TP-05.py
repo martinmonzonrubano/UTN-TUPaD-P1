703679850774a74ea5bbdcb52bd6644c29008a33
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-05-06 16:26:40 -0300

    Update MONZON_TP-05.py

--- 05 Funciones/MONZON_TP-05.py
+++ 05 Funciones/MONZON_TP-05.py
@@ -1,1 +1,72 @@
+#1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función range.
+lista1=list(range(4,101,4))
+print(lista1)
 
+#2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el
+#penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el
+#indexing con números negativos!
+
+lista_elem=["Mora", 22, 2.5, ["Programacion", "Matematica"], False]
+print(lista_elem[3])
+
+#3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por pantalla
+lista_vacia=[]
+lista_vacia.append("Hola")
+lista_vacia.append("Como")
+lista_vacia.append("Estas")
+print(lista_vacia)
+
+#4) Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”,
+#respectivamente. Imprimir la lista resultante por pantalla. ¡Puedes hacerlo como se muestra
+#en los videos o bien investigar cómo funciona el indexing con números negativos!
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1]="loro"
+animales[3]="oso"
+print(animales)
+
+#5) El codigo del ejercicio eliminara el numero mas alto de la lista, en ese caso en particular seria el 22.
+
+#6) Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por 
+# pantalla los dos primeros.
+lista2=list(range(10,31,5))
+print(lista2[0],lista2[1])
+
+#7) Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores
+#cualesquiera.
+
+autos=["Sedan", "Polo", "Suran"]
+autos[1]="Volkswagen"
+autos[2]="Audi"
+
+#8) Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append
+# directamente. Imprimir la lista resultante por pantalla.
+dobles=[]
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print(dobles)
+
+#9) Dada la lista “compras”, cuyos elementos representan los productos comprados por
+# diferentes clientes:
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+#a) Agregar "jugo" a la lista del tercer cliente usando append.
+#b) Reemplazar "fideos" por "tallarines" en la lista del segundo cliente.
+#c) Eliminar "pan" de la lista del primer cliente.
+#d) Imprimir la lista resultante por pantalla
+
+compras[2].append("jugo")
+compras[1][1]="tallarines"
+compras[0].remove("pan")
+print(compras)
+
+#10) Elaborar una lista anidada llamada “lista_anidada” que contenga los siguientes elementos:
+#● Posición lista_anidada[0]: 15
+#● Posición lista_anidada[1]: True
+#● Posición lista_anidada[2][0]: 25.5
+#● Posición lista_anidada[2][1]: 57.9
+#● Posición lista_anidada[2][2]: 30.6
+#● Posición lista_anidada[3]: False
+#Imprimir la lista resultante por pantalla.
+
+lista_anidada=[15, True, [25.5, 57.9, 30.6], False]
+print(lista_anidada)
