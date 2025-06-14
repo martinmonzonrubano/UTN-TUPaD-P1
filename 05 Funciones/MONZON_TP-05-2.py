2c9eb72b4f00a703e696921c27fd868c6f4e33cd
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-06-14 17:20:51 -0300

    Update MONZON_TP-05-2.py

--- 05 Funciones/MONZON_TP-05-2.py
+++ 05 Funciones/MONZON_TP-05-2.py
@@ -8,3 +8,16 @@ def imprimir_hola_mundo():
 #programa principal
 
 imprimir_hola_mundo()
+
+#2. Crear una función llamada saludar_usuario(nombre) que reciba
+#como parámetro un nombre y devuelva un saludo personalizado.
+#Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. Llamar a esta función desde el programa
+#principal solicitando el nombre al usuario.
+
+def saludar_usuario(nombre):
+    print(f"Hola {nombre}")
+
+#funcion principal
+
+nombre="Martin"
+saludar_usuario(nombre)
